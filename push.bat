893659dc3a25e82f4ba05111ca959a3917a22e82
1984tpearson <1984tpearson@gmail.com>
2026-05-30 15:09:12 +1000

    Create push.bat

---
+++ push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\Users\1984t\OneDrive\Documents\GitHub\fantgame
+"C:\Program Files\Git\cmd\git.exe" add worlds/aerdorn.js
+"C:\Program Files\Git\cmd\git.exe" commit -m "Briar-Town: shrink overworld footprint to 2x2, stub 50x60 interior"
+"C:\Program Files\Git\cmd\git.exe" push
