6f909b9855e95afd19e776771621e35969ba0dfb
1984tpearson <1984tpearson@gmail.com>
2026-05-31 17:56:10 +1000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   AI_PROXY_URL: 'https://keqzqhykfygplolcnxnn.supabase.co/functions/v1/ai-proxy',
   IMAGE_PROXY_URL: 'https://keqzqhykfygplolcnxnn.supabase.co/functions/v1/image-proxy',
 
-  TEXT_MODEL: 'nothingiisreal/mn-celeste-12b',
+  TEXT_MODEL: 'sao10k/l3-lunaris-8b',
 
   IMAGE_MODEL: 'sdxl_1024px',
   IMAGE_WIDTH: 1024,
