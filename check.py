6574c740b7b75de5291428ece982b65fa2f6481b
1984tpearson <1984tpearson@gmail.com>
2026-06-04 09:29:47 +1000

    g

--- check.py
+++ check.py
@@ -4,5 +4,5 @@ path = r'C:\Users\1984t\OneDrive\Documents\GitHub\fantgame\map-editor.html'
 with open(path, encoding='utf-8') as f:
     txt = f.read()
 
-idx = txt.find('function redrawCell(')
-print(txt[idx:idx+300])
+idx = txt.find('// Editor-placed building')
+print(txt[idx:idx+200])
