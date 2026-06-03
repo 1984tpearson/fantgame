f5dcc6ccce4963be8850177ca69dcaae2f6525ac
1984tpearson <1984tpearson@gmail.com>
2026-06-04 09:21:40 +1000

    yup

--- check.py
+++ check.py
@@ -4,9 +4,5 @@ path = r'C:\Users\1984t\OneDrive\Documents\GitHub\fantgame\map-editor.html'
 with open(path, encoding='utf-8') as f:
     txt = f.read()
 
-# Check what the terrain click handler looks like
-import re
-m = re.search(r"currentTerrainId=t\.id.*?;", txt)
-if m:
-    start = txt.rfind('\n', 0, m.start())
-    print(txt[start:start+300])
+idx = txt.find('function redrawCell(')
+print(txt[idx:idx+300])
