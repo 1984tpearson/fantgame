330915dcad629782bf63f83969138d50da6e2b54
1984tpearson <1984tpearson@gmail.com>
2026-06-04 09:10:30 +1000

    j

--- check.py
+++ check.py
@@ -1,7 +1,12 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
-with open(r'C:\Users\1984t\OneDrive\Documents\GitHub\fantgame\engine.js', encoding='utf-8') as f:
+path = r'C:\Users\1984t\OneDrive\Documents\GitHub\fantgame\map-editor.html'
+with open(path, encoding='utf-8') as f:
     txt = f.read()
 
-idx = txt.find('_drawObjSprite(ctx,tile')
-print(txt[idx-200:idx+130])
+# Check what the terrain click handler looks like
+import re
+m = re.search(r"currentTerrainId=t\.id.*?;", txt)
+if m:
+    start = txt.rfind('\n', 0, m.start())
+    print(txt[start:start+300])
