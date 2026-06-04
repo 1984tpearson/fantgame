f5c5b4df79a1d3d8002a9f597971d24d408825c0
1984tpearson <1984tpearson@gmail.com>
2026-06-04 22:38:45 +1000

    door

--- check.py
+++ check.py
@@ -1,17 +1,7 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
-
 with open('map-editor.html', encoding='utf-8') as f:
-    content = f.read()
-
-fixed = content.replace('\\x3c/script>\n</body>', '</script>\n</body>')
-
-if fixed == content:
-    print('ERROR: still not found')
-    # Show raw bytes around that area
-    idx = content.find('init();\n')
-    print(repr(content[idx:idx+40]))
-else:
-    with open('map-editor.html', 'w', encoding='utf-8') as f:
-        f.write(fixed)
-    print('Fixed.')
+    lines = f.readlines()
+for i, line in enumerate(lines, 1):
+    if any(k in line for k in ['function selectCell','function applyCell','function applyProps','btn-apply']):
+        print(f'{i}: {line.rstrip()[:100]}')
