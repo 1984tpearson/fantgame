612fe508f37ce8d3f2a02f2fb1da8593301f322f
1984tpearson <1984tpearson@gmail.com>
2026-06-04 13:40:28 +1000

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   NPC_IMAGE_WIDTH:    768,
   NPC_IMAGE_HEIGHT:   1024,
   NPC_IMAGE_GUIDANCE: 2.0,
-  NPC_IMAGE_SAMPLER:  'ddim',
+  NPC_IMAGE_SAMPLER:  'auto',
   NPC_IMAGE_STYLE_SUFFIX: 'gritty fantasy+++, Full body shot++, (Comic realism style+, black ink outlines, flat cell shading, painterly colouring)++:',
   NPC_IMAGE_NEGATIVE: 'realistic, photo, modern, manga, anime, japanese comic style, young, pretty , Smooth shading, texture, extra cloth--, loose fabric--, monochrome, sketch, tattoos, big mouth, big smile, sci-fi, superhero, muscles, muscular, extra limbs, badly drawn hands',
 
