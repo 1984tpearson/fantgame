2fa4453751f24f175293e8efc53ea4bd54f06f32
1984tpearson <1984tpearson@gmail.com>
2026-06-05 11:40:25 +1000

    pooh

--- check.py
+++ check.py
@@ -3,5 +3,5 @@ sys.stdout.reconfigure(encoding='utf-8')
 with open('map-editor.html', encoding='utf-8') as f:
     lines = f.readlines()
 for i, line in enumerate(lines, 1):
-    if 'makeTerrain' in line or ('getTile' in line and 'editor' in lines[i-2].lower() if i>2 else False):
+    if 'function buildSettlementList' in line:
         print(f'{i}: {line.rstrip()[:100]}')
