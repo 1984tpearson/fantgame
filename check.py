a0fd34079240716e6bd3a7e3871660838975fea2
1984tpearson <1984tpearson@gmail.com>
2026-06-05 12:11:22 +1000

    f

--- check.py
+++ check.py
@@ -1,7 +1,14 @@
-import sys
+import sys, re
 sys.stdout.reconfigure(encoding='utf-8')
-with open('worlds/aerdorn.js', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines, 1):
-    if 'NPC_TEMPLATES' in line or 'FACTIONS' in line or ('}' in line and ';' in line and i > 100):
-        print(f'{i}: {line.rstrip()[:80]}')
+
+with open('map-editor.html', encoding='utf-8') as f:
+    content = f.read()
+
+script_start = content.find('\n<script>\n')
+script_end = content.rfind('</script>')
+js = content[script_start:script_end]
+
+for m in re.finditer(r'</script', js, re.IGNORECASE):
+    pos = m.start()
+    line = js[:pos].count('\n') + 1
+    print(f'line {line}: {js[max(0,pos-40):pos+20]!r}')
