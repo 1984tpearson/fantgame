069084cdfa87400a0e9080c9493c47886e5dd93e
1984tpearson <1984tpearson@gmail.com>
2026-06-06 13:46:55 +1000

    dar

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
-with open('map-editor.html', encoding='utf-8') as f:
+with open('mapforge.js', encoding='utf-8') as f:
     lines = f.readlines()
 for i, line in enumerate(lines, 1):
-    if 'southRoom' in line or 'Entrance' in line or ('H-1' in line and 'door' in line):
-        print(f'{i}: {line.rstrip()[:100]}')
+    if 'rocky' in line and '255' in line:
+        print(f'{i}: {line.rstrip()[:80]}')
