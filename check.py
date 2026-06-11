c9f3b8aea3ce9390d8da7630d034234ab395a6df
1984tpearson <1984tpearson@gmail.com>
2026-06-11 19:30:55 +1000

    yflip

--- check.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
-with open('mapforge.js', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines, 1):
-    if 'rocky' in line and '255' in line:
-        print(f'{i}: {line.rstrip()[:80]}')
