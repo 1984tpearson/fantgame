eeaf996c1dc32754197e04e59a0b1a899b6855fa
1984tpearson <1984tpearson@gmail.com>
2026-06-11 19:53:53 +1000

    Map editor: update worlds/settlements/carrow_town/map.js

--- worlds/settlements/carrow_town/map.js
+++ worlds/settlements/carrow_town/map.js
@@ -19,6 +19,7 @@
   t(9,7,'grass','', {object:"bush_lg",objVariant:0});
   t(10,7,'grass','', {object:"path_v",objVariant:0});
   t(1,6,'grass','', {object:"signpost",objVariant:0});
+  t(5,6,'grass','', {object:"well",objVariant:0});
   t(6,6,'grass','', {object:"logpile",objVariant:0});
   t(10,6,'grass','', {object:"path_v",objVariant:0});
   t(0,5,'street','');
@@ -36,20 +37,30 @@
   t(2,4,'grass','', {object:"_part",anchor:"2,2"});
   t(3,4,'grass','', {object:"_part",anchor:"2,2"});
   t(4,4,'grass','', {object:"_part",anchor:"2,2"});
-  t(5,4,'grass','');
-  t(6,4,'grass','', {object:"campfire",objVariant:0});
+  t(5,4,'grass','', {object:"haystack",objVariant:0});
+  t(8,4,'grass','', {object:"path_v"});
   t(9,4,'grass','', {object:"trough",rotation:180,objVariant:0});
   t(2,3,'grass','', {object:"_part",anchor:"2,2"});
   t(3,3,'grass','', {object:"_part",anchor:"2,2"});
   t(4,3,'grass','', {object:"_part",anchor:"2,2"});
-  t(5,3,'grass','');
+  t(8,3,'grass','', {object:"path_v"});
   t(2,2,'building','', {interiorType:"house",bldgType:"standard",bldgW:3,bldgH:3,bldgPxW:60,bldgPxH:60,roofStyle:"red",roofShape:"hip",bldgPattern:"default",bldgYard:"bottom"});
   t(3,2,'grass','', {object:"_part",anchor:"2,2"});
   t(4,2,'grass','', {object:"_part",anchor:"2,2"});
-  t(6,2,'grass','', {object:"flowers",objVariant:0});
+  t(5,2,'grass','', {object:"path_h"});
+  t(6,2,'grass','', {object:"path_h",objVariant:0});
+  t(7,2,'grass','', {object:"path_h"});
+  t(8,2,'grass','', {object:"path_csw",objVariant:0});
+  t(2,1,'mud','');
+  t(3,1,'mud','');
+  t(4,1,'mud','');
+  t(2,0,'mud','', {object:"coop",objVariant:0});
+  t(3,0,'mud','');
+  t(4,0,'mud','');
   t(5,0,'mud','', {objVariant:0});
   t(6,0,'mud','', {objVariant:0});
   t(7,0,'grass','');
+  t(9,0,'grass','', {object:"tent",objVariant:0});
 
   window.SETTLEMENTS["carrow_town"] = {
     map: m,
